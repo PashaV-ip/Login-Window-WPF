902b0ae911436d6a9143c43d53062b48638edc0c
Vanshev <pav-alva@ya.ru>
2022-09-11 14:26:30 +0300

    Добавьте файлы проекта.

---
+++ Login Window/Classes/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Window
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User(string Login, string Password)
+        {
+            this.Login = Login;
+            this.Password = Password;
+        }
+    }
+}
---
+++ Login Window/Classes/Validation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Window
+{
+    public static class Validation
+    {
+        public static bool Validation_Method(string Login, string Password)
+        {
+            User user1 = new User("Kostia", "123456");
+            if(Login == user1.Login && Password == user1.Password)
+            return true;
+            return false;
+        }
+    }
+}
